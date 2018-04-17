20f989f0df7d227aad1ffbf912719134c0a72fc8
Gen Liu <gerald_721@hotmail.com>
2018-04-18 00:51:11 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'CANVAS_RENDERER': JSON.stringify(true),
+      'WEBGL_RENDERER': JSON.stringify(true)
+    })
+  ]
+}
